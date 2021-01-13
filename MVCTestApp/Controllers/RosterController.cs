f5d479d03485c90ec39966008c9e496c9ef4b89f
House of Sixten <guuonearth@yahoo.com>
2021-01-13 11:39:20 -0800

    Adding an MVCTestApp to git for the first time

---
+++ MVCTestApp/Controllers/RosterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestApp.Controllers
+{
+    public class RosterController : Controller
+    {
+        private List<Models.Employee> emp;
+
+        public RosterController()
+        {
+            emp = new List<Models.Employee>();
+
+            emp.Add(new Models.Employee(Models.OperatorLevel.Elite,
+                                        Models.OperatorClass.Supporter,
+                                        "Angelina",
+                                        "Airborne messenger with the power to control gravity",
+                                        "angelina.jpg"));
+
+            emp.Add(new Models.Employee(Models.OperatorLevel.Senior,
+                                        Models.OperatorClass.Medic,
+                                        "Perfumer",
+                                        "Gardener whose aromatherapy heals wounds",
+                                        "perfumer.jpg"));
+
+            emp.Add(new Models.Employee(Models.OperatorLevel.Veteran,
+                                        Models.OperatorClass.Medic,
+                                        "Myrrh",
+                                        "Potato fox alchemist",
+                                        "myrrh.jpg"));
+        }
+
+        public IActionResult Index()
+        {
+            return View(emp);
+        }
+    }
+}
---
+++ MVCTestApp/Models/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestApp.Models
+{
+    public enum OperatorLevel
+    {
+        Robot = 1,
+        Trainee = 2,
+        Junior = 3,
+        Veteran = 4,
+        Senior = 5,
+        Elite = 6        
+    }
+
+    public enum OperatorClass
+    {
+        Guard = 1,
+        Defender = 2,
+        Sniper = 3,
+        Caster = 4,
+        Medic = 5,
+        Supporter = 6,
+        Specialist = 7
+    }
+
+    public class Employee
+    {
+        public OperatorLevel Level { get; set; }
+
+        public OperatorClass OperatorClass { get; set; }
+
+        public string CodeName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+
+        public Employee(OperatorLevel level, OperatorClass operatorClass, string codeName, string title, string image)
+        {
+            Level = level;
+            OperatorClass = operatorClass;
+            CodeName = codeName;
+            Title = title;
+            Image = image;
+        }
+    }
+}
